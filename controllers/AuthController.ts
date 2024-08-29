acfd8a0302f45ba80b7fb22408d8ea5d6e6bce3d
revaananda <aarevaananda@gmail.com>
2024-08-29 10:59:16 +0700

    change db tables name

--- controllers/AuthController.ts
+++ controllers/AuthController.ts
@@ -1,17 +1,11 @@
 import { Request, Response } from "express";
-import { PrismaClient, User } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import crypto from "crypto";
 
 const prisma = new PrismaClient();
 
-// exp req body
-//{
-//     "username":"testuser",
-//     "half_nonce":"932eweqf"
-//     }
-// generate random alphanumeric string
+// Generate random alphanumeric string
 function generateRandomString(length: number): string {
-  //func dari WA
   const charset = "abcdefghijklmnopqrstuvwxyz0123456789";
   let result = "";
   for (let i = 0; i < length; i++) {
@@ -30,15 +24,15 @@ export async function handleLoginRequest(
 
     console.log(req.body);
 
-    //validasi
+    // Validation
     if (!username || !half_nonce) {
       res.status(400).json({ error: "Invalid input" });
       return;
     }
+
     const user = await prisma.user.findUnique({
-      //
       where: { username },
-      select: { id: true, salt: true, saltedPassword: true },
+      select: { id: true, salt: true, salted_password: true },
     });
 
     if (!user) {
@@ -46,22 +40,22 @@ export async function handleLoginRequest(
       return;
     }
 
-    // Genrt nonce1
+    // Generate nonce1
     const nonce1 = generateRandomString(8);
     const fullNonce = `${half_nonce}${nonce1}`;
 
     // Generate challenge_response
     const challengeResponse = crypto
-      .createHmac("sha256", user.saltedPassword)
+      .createHmac("sha256", user.salted_password)
       .update(fullNonce)
       .digest("base64");
 
     // Save challenge_response to DB
-    await prisma.challengeResponse.create({
+    await prisma.challenge_response.create({
       data: {
-        fullNonce,
-        userId: user.id,
-        challengeResponse,
+        full_nonce: fullNonce,
+        user_id: user.id,
+        challenge_response: challengeResponse,
       },
     });
 
@@ -71,95 +65,10 @@ export async function handleLoginRequest(
       challenge_response: challengeResponse,
     });
   } catch (e) {
-    console.error("Error handling login request:", e, req.body );
-    res.status(500).json({ message:"Internal server error:" , e , reqBody: req.body });
-
+    console.error("Error handling login request:", e, req.body);
+    res.status(500).json({ message: "Internal server error", e, reqBody: req.body });
   }
 }
-
-// export async function handleChallengeResponseVerification(
-//   req: Request,
-//   res: Response
-// ): Promise<void> {
-//   try {
-//     const { full_nonce, challenge_response } = req.body;
-
-//     //debug
-//     console.log(req.body);
-
-//     if (!full_nonce || !challenge_response) {
-//       res.status(400).json({ error: "Invalid input" });
-//       return;
-//     }
-
-//     // Find the challenge response on DB
-//     const challenge = await prisma.challengeResponse.findUnique({
-//       where: { fullNonce: full_nonce },
-//       include: { user: true }, // Include user relationship
-//     });
-
-//     if (!challenge) {
-//       res.status(401).json({ error: "Challenge not valid" });
-//       return;
-//     }
-
-//     // Verify  challenge res
-//     const isValid =
-//       crypto
-//         .createHmac("sha256", challenge.user.saltedPassword)
-//         .update(full_nonce)
-//         .digest("base64") === challenge_response;
-
-//     if (isValid) {
-//       // Generate session ID dan nonce2
-//       const sessionId = generateRandomString(16);
-//       const nonce2 = generateRandomString(8);
-//       const sessionSecret = crypto
-//         .createHmac("sha256", challenge.user.saltedPassword)
-//         .update(`${full_nonce}${nonce2}`)
-//         .digest("base64");
-
-   
-//       const timestamp = Math.floor(Date.now() / 1000); // Timestamp in secons
-
-//       //if challenge response valid:
-//       // generate session id - 16 random alphanumeric lowercase characters
-//       // generate nonce2 = 8 random alphanumeric lowercase characters
-//       // session secret = hmac-sha256(key=salted password, message = full nonce + nonce2
-
-//       //save session id, session secret, user id, tstamp to Session db
-
-//       // save Session db
-//       const newSession = await prisma.session.create({
-//         data: {
-//           sessionId,
-//           userId: challenge.userId,
-//           sessionSecret,
-//           tstamp: timestamp,
-//           st: 1, // Asumsi status 1 untuk session yang aktif
-//         },
-//       });
-      
-//       // Tampilkan data session yang baru saja disimpan
-//       console.log(newSession);
-    
-//       res.status(200).json({
-//         message: "Challenge response is valid",
-//         sessionId,
-//         nonce2,
-//         userData;
-//       });
-
-   
-
-//     } else {
-//       res.status(400).json({ message: "Invalid challenge response" });
-//     }
-//   } catch (error) {
-//     console.error("Error verifying challenge response:", error);
-//     res.status(500).json({ error: "Internal server error" });
-//   }
-// }
 
 export async function handleChallengeResponseVerification(
   req: Request,
@@ -176,9 +85,9 @@ export async function handleChallengeResponseVerification(
       return;
     }
 
-    // Find the challenge response on DB
-    const challenge = await prisma.challengeResponse.findUnique({
-      where: { fullNonce: full_nonce },
+    // Find the challenge response in DB
+    const challenge = await prisma.challenge_response.findUnique({
+      where: { full_nonce },
       include: { user: true }, // Include user relationship
     });
 
@@ -190,16 +99,16 @@ export async function handleChallengeResponseVerification(
     // Verify challenge response
     const isValid =
       crypto
-        .createHmac("sha256", challenge.user.saltedPassword)
+        .createHmac("sha256", challenge.user.salted_password)
         .update(full_nonce)
         .digest("base64") === challenge_response;
 
     if (isValid) {
-      // Gnrt session ID and nonce2
-      const sessionId = generateRandomString(16);
+      // Generate session ID and nonce2
+      const session_id = generateRandomString(16);
       const nonce2 = generateRandomString(8);
-      const sessionSecret = crypto
-        .createHmac("sha256", challenge.user.saltedPassword)
+      const session_secret = crypto
+        .createHmac("sha256", challenge.user.salted_password)
         .update(`${full_nonce}${nonce2}`)
         .digest("base64");
 
@@ -208,37 +117,35 @@ export async function handleChallengeResponseVerification(
       // Save Session to DB
       const newSession = await prisma.session.create({
         data: {
-          sessionId,
-          userId: challenge.userId,
-          sessionSecret,
+          session_id,
+          user_id: challenge.user_id,
+          session_secret,
           tstamp: timestamp,
           st: 1, 
         },
       });
 
-
       const userData = {
         id: challenge.user.id.toString(), // Convert BigInt to string
         username: challenge.user.username,
         salt: challenge.user.salt,
-        saltedPassword : challenge.user.saltedPassword
+        salted_password: challenge.user.salted_password
       };
 
       // Send response to frontend
       res.status(200).json({
         message: "Challenge response is valid",
-        sessionId,
+        session_id,
         nonce2,
-        userData, 
+        userData,
       });
 
     } else {
-      console.error("Invalid challenge response", req.body );
+      console.error("Invalid challenge response", req.body);
       res.status(400).json({ message: "Invalid challenge response", reqBody: req.body });
-
     }
   } catch (e) {
-    console.error("Error verifying challenge response:", e, req.body );
-    res.status(500).json({ error: "Internal server error" , e , reqBody: req.body });
+    console.error("Error verifying challenge response:", e, req.body);
+    res.status(500).json({ error: "Internal server error", e, reqBody: req.body });
   }
 }
---
+++ prisma/migrations/20240829035146_rename_tables_and_fields/migration.sql
@@ -0,0 +1,67 @@
+/*
+  Warnings:
+
+  - You are about to drop the `ChallengeResponse` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Session` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "ChallengeResponse" DROP CONSTRAINT "ChallengeResponse_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Session" DROP CONSTRAINT "Session_userId_fkey";
+
+-- DropTable
+DROP TABLE "ChallengeResponse";
+
+-- DropTable
+DROP TABLE "Session";
+
+-- DropTable
+DROP TABLE "User";
+
+-- CreateTable
+CREATE TABLE "challenge_response" (
+    "full_nonce" VARCHAR(16) NOT NULL,
+    "user_id" BIGINT NOT NULL,
+    "challenge_response" VARCHAR(128) NOT NULL,
+
+    CONSTRAINT "challenge_response_pkey" PRIMARY KEY ("full_nonce")
+);
+
+-- CreateTable
+CREATE TABLE "session" (
+    "session_id" VARCHAR(16) NOT NULL,
+    "user_id" BIGINT NOT NULL,
+    "session_secret" VARCHAR(128) NOT NULL,
+    "tstamp" BIGINT NOT NULL,
+    "st" INTEGER NOT NULL,
+
+    CONSTRAINT "session_pkey" PRIMARY KEY ("session_id")
+);
+
+-- CreateTable
+CREATE TABLE "user" (
+    "id" BIGSERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "salt" TEXT NOT NULL,
+    "salted_password" TEXT NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "challenge_response_user_id_idx" ON "challenge_response"("user_id");
+
+-- CreateIndex
+CREATE INDEX "user_id_index" ON "session"("user_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_username_key" ON "user"("username");
+
+-- AddForeignKey
+ALTER TABLE "challenge_response" ADD CONSTRAINT "challenge_response_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "session" ADD CONSTRAINT "session_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
