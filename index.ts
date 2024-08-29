d32438212fd931b2d22b75aaec2510fd6900e017
revaananda <aarevaananda@gmail.com>
2024-08-29 10:03:54 +0700

    tambah check db

--- index.ts
+++ index.ts
@@ -1,23 +1,35 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
+
+
 import authRoutes from './routes/AuthRoutes';
 
- import swaggerUi  from 'swagger-ui-express';
+import swaggerUi  from 'swagger-ui-express';
 
- import swaggerDocument from './swagger.json';
+import swaggerDocument from './swagger.json';
 
 
 
+const prisma = new PrismaClient();
 const app = express()
 app.use(express.json());
 
 
-//middlewares
 
 app.use(express.urlencoded({extended: true}))
 
-app.get('/', (req, res) => {
-    res.send('Test')
-})
+app.get('/', async (req, res) => {
+    try {
+        await prisma.$connect();
+        res.send('Terhubung ke database');
+    } catch (error) {
+        console.error('Tidak dapat terhubung ke database:', error);
+        res.status(500).send('Tidak dapat terhubung ke database');
+    } finally {
+        await prisma.$disconnect();
+    }
+});
+
 
  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
