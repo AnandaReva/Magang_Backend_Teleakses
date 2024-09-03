0b4344bf5b029bab7b5825f4aa9faed813ecf506
Ananadareva <aarevaaananda@gmail.com>
2024-09-03 09:47:58 +0700

    split project 2

---
+++ Report_service/controllers/reportController.ts
@@ -0,0 +1,184 @@
+import { Request, Response } from 'express';
+import { PrismaClient } from "@prisma/client";
+import dotenv from 'dotenv'
+import generateTimestamp from '../utils/generateTimeStamp'
+import createHMACSHA256Hash from "../utils/createHMACSHA256Hash";
+const prisma = new PrismaClient();
+
+dotenv.config();
+
+
+const validateRequestHash = async (req: Request): Promise<boolean> => {
+    const timeStamp = new Date().toISOString();
+    console.log('Executing method: validateRequestHash');
+    try {
+        const sessionId = req.headers['ecwx-session-id'] as string || '';
+        const hashReceived = req.headers['ecwx-hash'] as string || '';
+        console.log("Session ID Received:", sessionId);
+        console.log("Hash Received:", hashReceived);
+        // Validate 
+        const missingFields: string[] = [];
+        if (!sessionId) missingFields.push('session_id');
+        if (!hashReceived) missingFields.push('hash');
+        if (missingFields.length > 0) {
+            console.error(`[${timeStamp}] Missing fields: ${missingFields.join(', ')}\nRequest sent: ${JSON.stringify(req.body)}`);
+            return false;
+        }
+
+        const sessionData = await prisma.session.findUnique({ where: { session_id: sessionId } });
+        if (!sessionData) {
+            console.error(`Session data with id = [ ${sessionId} ] not found in database \n`)
+            return false;
+        }
+
+        const sessionSecret = sessionData['session_secret'];
+        const postBody = req.body;
+
+        const postBodyString = JSON.stringify(postBody);
+        const hashExpected = createHMACSHA256Hash(postBodyString, sessionSecret.toString());
+        console.log("Session Secret from DB:", sessionSecret);
+        console.log("Post Body from FE (stringify):", postBodyString);
+        console.log(`Expected Hash: [${hashExpected}]`);
+        console.log(`Received Hash from header: [${hashReceived}]`);
+
+        if (hashReceived === hashExpected) {
+            return true;
+        } else {
+            return false;
+        }
+    } catch (error) {
+        console.error(`[${timeStamp}] Error while validating hash:`, error);
+        return false;
+    }
+};
+
+export const getBotConversationHistoryTable = async (req: Request, res: Response) => {
+    const realBackendURL = process.env.endpoint1 ?? '';
+    console.log(`Real Bachend URL: ${realBackendURL}`);
+    const timeStamp = generateTimestamp();
+
+    // Check if the URL is defined 
+    if (!realBackendURL) {
+        res.status(500).json({ error: 'Backend URL is not defined' });
+        console.error(`[${timeStamp}] Response sent: res.status(500).json({ error: "Backend URL is not defined" }); Backend URL is not defined`);
+        return;
+    }
+    const isHashValid = await validateRequestHash(req);
+    if (!isHashValid) {
+        res.status(401).json({
+            error_code: `Hash not valid`
+        });
+        console.error(`[${timeStamp}]response sent :  res.status(401).json({error: "Hash not valid"}); Hash not valid`);
+        return;
+    }
+    //if hash valid 
+    console.log('Hash is valid');
+    console.log(`[${timeStamp} continuing request to real backend url: ${realBackendURL}]`);
+    try {
+
+        // Send to real backend
+        const backendResponse = await fetch(realBackendURL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'ecwx-session-id': req.headers['ecwx-session-id'] as string,
+                'ecwx-hash': req.headers['ecwx-hash'] as string,
+            },
+            body: JSON.stringify(req.body),
+        });
+        const responseData = await backendResponse.json();
+        const realbackendResStatus = backendResponse.status;
+        res.status(realbackendResStatus).json(responseData);
+        console.log(`Response real backend: res.status(${realbackendResStatus}).json(${JSON.stringify(responseData)});`,);
+        console.log(`Response sent res.status(${realbackendResStatus}).json(${JSON.stringify(responseData)});`,);
+
+    } catch (e) {
+        console.error(`[${timeStamp}] Error forwarding request to backend:' ${e}`);
+        res.status(500).json({ error: 'Failed to communicate with backend' });
+    }
+}
+
+
+export const getBotExecutiveSummary = async (req: Request, res: Response) => {
+    const realBackendURL = process.env.endpoint2 ?? "";
+    console.log(`Real Bachend URL: ${realBackendURL}`);
+    const timeStamp = generateTimestamp();
+
+    // Check if the URL is defined 
+    if (!realBackendURL) {
+        res.status(500).json({ error: 'Backend URL is not defined' });
+        console.error(`[${timeStamp}] Response sent: res.status(500).json({ error: "Backend URL is not defined" }); Backend URL is not defined`);
+        return;
+    }
+
+    const isHashValid = await validateRequestHash(req);
+    if (!isHashValid) {
+        res.status(401).json({ error_code: 'Hash not valid' });
+        console.error(`[${timeStamp}]response sent :  res.status(401).json({error: "Hash not valid"}); Hash not valid`);
+        return;
+    }
+    console.log('Hash is valid');
+    console.log(`[${timeStamp} continuing request to real backend url: ${realBackendURL}]`);
+    try {
+        const backendResponse = await fetch(realBackendURL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'ecwx-session-id': req.headers['ecwx-session-id'] as string,
+                'ecwx-hash': req.headers['ecwx-hash'] as string,
+            },
+            body: JSON.stringify(req.body),
+        });
+        const responseData = await backendResponse.json();
+        const realbackendResStatus = backendResponse.status;
+        res.status(realbackendResStatus).json(responseData);
+        console.log(`Response real backend: res.status(${realbackendResStatus}).json(${JSON.stringify(responseData)});`,);
+        console.log(`Response sent res.status(${realbackendResStatus}).json(${JSON.stringify(responseData)});`,);
+    } catch (e) {
+        console.error(`[${timeStamp}] Error forwarding request to backend: ${e}`);
+        res.status(500).json({ error: 'Failed to communicate with backend' });
+    }
+}
+
+export const getBotConversationTopicChart = async (req: Request, res: Response) => {
+
+    const timeStamp = generateTimestamp();
+    const realBackendURL = process.env.endpoint3 ?? "";
+    console.log(`Real Bachend URL: ${realBackendURL}`);
+    // Check if the URL is defined 
+    if (!realBackendURL) {
+        res.status(500).json({ error: 'Backend URL is not defined' });
+        console.error(`[${timeStamp}] Response sent: res.status(500).json({ error: "Backend URL is not defined" }); Backend URL is not defined`);
+        return;
+    }
+    const isHashValid = await validateRequestHash(req);
+    if (!isHashValid) {
+        res.status(401).json({ error_code: 'Hash not valid' });
+        console.error(`[${timeStamp}]response sent :  res.status(401).json({error: "Hash not valid"}); Hash not valid`);
+        return;
+    }
+    console.log('Hash is valid');
+    console.log(`[${timeStamp}] continuing request to real backend url: [${realBackendURL}]`);
+    try {
+        const backendResponse = await fetch(realBackendURL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'ecwx-session-id': req.headers['ecwx-session-id'] as string,
+                'ecwx-hash': req.headers['ecwx-hash'] as string,
+            },
+            body: JSON.stringify(req.body),
+        });
+        const responseData = await backendResponse.json();
+        const realbackendResStatus = backendResponse.status;
+        res.status(realbackendResStatus).json(responseData);
+        console.log(`Response real backend: res.status(${realbackendResStatus}).json(${JSON.stringify(responseData)});`,);
+        console.log(`Response sent res.status(${realbackendResStatus}).json(${JSON.stringify(responseData)});`,);
+    } catch (e) {
+        console.error(`[${timeStamp}] Error forwarding request to backend: ${e}`);
+        res.status(500).json({ error: 'Failed to communicate with backend' });
+    }
+}
+
+
+
